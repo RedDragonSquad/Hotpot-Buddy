d3c15cbf4b388ffdbb5dd77f1702a0195ca6c492
Keerysanth Sribaskaran <keerush@live.com>
2022-08-07 16:42:05 -0400

    Add useIngredientsList hook

---
+++ client/src/pages/pot-instance/hooks/useIngredientsList.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import { Ingredient } from 'pages/pot-instance/models';
+
+const useIngredientsList = (): {
+  loading: Boolean;
+  data: Ingredient[];
+} => {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const fetchIngredients = () => {
+    fetch('./mockData/ingredients.json', {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      }
+    })
+      .then((res) => res.json())
+      .then((jsonData) => {
+        setLoading(false);
+        setData(jsonData);
+      });
+  };
+
+  useEffect(() => {
+    fetchIngredients();
+  }, []);
+
+  return { loading, data };
+};
+
+export default useIngredientsList;
