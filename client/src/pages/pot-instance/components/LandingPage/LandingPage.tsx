d7876a014ba62b869276024a0a5dfcd26b535b7e
AndrewLin95 <andrew.lin88@hotmail.com>
2022-08-22 20:39:09 -0400

    updated guard clause to return the positve case first

--- client/src/pages/pot-instance/components/LandingPage/LandingPage.tsx
+++ client/src/pages/pot-instance/components/LandingPage/LandingPage.tsx
@@ -15,78 +15,78 @@ const LandingPage: FC<Props> = ({
   startHotPot,
   hotpotStart
 }) => {
-  if (!hotpotStart) {
-    return (
-      <div id={styles.selectPotContainer}>
-        <img
-          id={styles.hotpotBackground}
-          // eslint-disable-next-line global-require
-          src={require('./assets/hotPotBackground.jpg')}
-          alt="hotpot background"
-        />
-        <div id={styles.selectPot}>
-          <div className={styles.selectItem}>
-            Select Pot Type
-            <div className={styles.selectType}>
-              <button
-                type="button"
-                onClick={() => {
-                  updatePotType(1);
-                }}
-                className={styles.flavors}
-              >
-                One Flavor
-              </button>
-              <button
-                type="button"
-                onClick={() => {
-                  updatePotType(2);
-                }}
-                className={styles.flavors}
-              >
-                Two Flavors
-              </button>
-              <button
-                type="button"
-                onClick={() => {
-                  updatePotType(4);
-                }}
-                className={styles.flavors}
-              >
-                Four Flavors
-              </button>
-            </div>
-          </div>
-          <div className={styles.selectBroth}>
-            Select Broth
-            <div className={styles.selectBrothType}>
-              <button type="button" className={styles.soupbases}>
-                Sukiyaki Soupbase
-              </button>
-              <button type="button" className={styles.soupbases}>
-                Spicy Soupbase
-              </button>
-              <button type="button" className={styles.soupbases}>
-                Ox Bone Soupbase
-              </button>
-              <button type="button" className={styles.soupbases}>
-                Tomato Soupbase
-              </button>
-            </div>
-          </div>
-          <Button
-            onClick={startHotPot}
-            type="button"
-            variant="contained"
-            id={styles.startHotPotBtn}
-          >
-            Start
-          </Button>
-        </div>
-      </div>
-    );
+  if (hotpotStart) {
+    return <>error</>;
   }
-  return <>error</>;
+  return (
+    <div id={styles.selectPotContainer}>
+      <img
+        id={styles.hotpotBackground}
+        // eslint-disable-next-line global-require
+        src={require('./assets/hotPotBackground.jpg')}
+        alt="hotpot background"
+      />
+      <div id={styles.selectPot}>
+        <div className={styles.selectItem}>
+          Select Pot Type
+          <div className={styles.selectType}>
+            <button
+              type="button"
+              onClick={() => {
+                updatePotType(1);
+              }}
+              className={styles.flavors}
+            >
+              One Flavor
+            </button>
+            <button
+              type="button"
+              onClick={() => {
+                updatePotType(2);
+              }}
+              className={styles.flavors}
+            >
+              Two Flavors
+            </button>
+            <button
+              type="button"
+              onClick={() => {
+                updatePotType(4);
+              }}
+              className={styles.flavors}
+            >
+              Four Flavors
+            </button>
+          </div>
+        </div>
+        <div className={styles.selectBroth}>
+          Select Broth
+          <div className={styles.selectBrothType}>
+            <button type="button" className={styles.soupbases}>
+              Sukiyaki Soupbase
+            </button>
+            <button type="button" className={styles.soupbases}>
+              Spicy Soupbase
+            </button>
+            <button type="button" className={styles.soupbases}>
+              Ox Bone Soupbase
+            </button>
+            <button type="button" className={styles.soupbases}>
+              Tomato Soupbase
+            </button>
+          </div>
+        </div>
+        <Button
+          onClick={startHotPot}
+          type="button"
+          variant="contained"
+          id={styles.startHotPotBtn}
+        >
+          Start
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
