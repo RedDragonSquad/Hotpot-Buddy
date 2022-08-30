bffcfad8ab113db419024f8f54287a281f3739e1
AndrewLin95 <andrew.lin88@hotmail.com>
2022-08-30 18:51:52 -0400

    added date-fns

--- client/src/pages/pot-instance/components/CompletedItems/CompletedItems.tsx
+++ client/src/pages/pot-instance/components/CompletedItems/CompletedItems.tsx
@@ -9,15 +9,10 @@ const CompletedItems: FC<Props> = ({ cookedPotContent }) => {
   return (
     <>
       {Object.entries(cookedPotContent).map(([key, value]) => {
-        const date = new Date(value.endTime * 1000);
-        const hours = date.getHours();
-        const minutes = date.getMinutes();
-        const seconds = date.getSeconds();
-        const formattedTime = `${hours}:${minutes}:${seconds}`;
         return (
           <div key={key}>
             <div>Item: {value.name}</div>
-            <div>Consumed Time: {formattedTime}</div>
+            <div>Consumed Time: {}</div>
           </div>
         );
       })}
