b35bc3bdae7577540ecb5e4985015c3e2f12abed
AndrewLin95 <andrew.lin88@hotmail.com>
2022-08-30 19:14:14 -0400

    added date-fns formatted time

--- client/src/pages/pot-instance/components/CompletedItems/CompletedItems.tsx
+++ client/src/pages/pot-instance/components/CompletedItems/CompletedItems.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { format } from 'date-fns';
 import { PotContent } from 'pages/pot-instance/models';
 
 interface Props {
@@ -9,10 +10,13 @@ const CompletedItems: FC<Props> = ({ cookedPotContent }) => {
   return (
     <>
       {Object.entries(cookedPotContent).map(([key, value]) => {
+        // date format 	12:00:00 AM was chosen. we can update to a custom format in the future if we choose. https://date-fns.org/v2.29.2/docs/format
+        const formattedTime = format(new Date(value.endTime * 1000), 'pp');
         return (
           <div key={key}>
-            <div>Item: {value.name}</div>
-            <div>Consumed Time: {}</div>
+            <div>
+              {value.name} || Consumed Time: {formattedTime}
+            </div>
           </div>
         );
       })}
