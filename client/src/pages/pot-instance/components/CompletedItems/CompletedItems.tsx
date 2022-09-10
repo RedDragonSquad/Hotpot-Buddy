70f73ebadf3352d95a2eb378e6728815262ed9b0
AndrewLin95 <andrew.lin88@hotmail.com>
2022-09-10 14:27:49 -0400

    added icon for completed item list

--- client/src/pages/pot-instance/components/CompletedItems/CompletedItems.tsx
+++ client/src/pages/pot-instance/components/CompletedItems/CompletedItems.tsx
@@ -1,4 +1,7 @@
 import { FC } from 'react';
+import { IconButton } from '@mui/material';
+import RestaurantIcon from '@mui/icons-material/Restaurant';
+import { format } from 'date-fns';
 import { PotContent } from 'pages/pot-instance/models';
 import styles from './styles.module.css';
 
@@ -10,12 +13,17 @@ const CompletedItems: FC<Props> = ({ cookedPotContent }) => {
   return (
     <>
       {Object.entries(cookedPotContent).map(([key, value]) => {
+        const formattedTime = format(new Date(value.endTime * 1000), 'p');
         return (
           <div className={styles.timerComponent} key={key}>
             <div className={styles.timerInformation}>
               <div className={styles.itemName}>{value.name}</div>
-              <div>Finished at: </div>
+              <div>Finished at: {formattedTime} </div>
             </div>
+
+            <IconButton key={key} color="primary">
+              <RestaurantIcon fontSize="small" />
+            </IconButton>
           </div>
         );
       })}
