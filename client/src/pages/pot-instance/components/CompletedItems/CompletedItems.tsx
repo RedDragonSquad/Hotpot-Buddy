3f2ee200883f45a24e68e5fd91c1b64899bee6af
AndrewLin95 <andrew.lin88@hotmail.com>
2022-09-10 14:13:25 -0400

    added date-fns for date formatting

--- client/src/pages/pot-instance/components/CompletedItems/CompletedItems.tsx
+++ client/src/pages/pot-instance/components/CompletedItems/CompletedItems.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import { PotContent } from 'pages/pot-instance/models';
+import styles from './styles.module.css';
 
 interface Props {
   cookedPotContent: PotContent[];
@@ -9,7 +10,14 @@ const CompletedItems: FC<Props> = ({ cookedPotContent }) => {
   return (
     <>
       {Object.entries(cookedPotContent).map(([key, value]) => {
-        return <div key={key}>{value.name}</div>;
+        return (
+          <div className={styles.timerComponent} key={key}>
+            <div className={styles.timerInformation}>
+              <div className={styles.itemName}>{value.name}</div>
+              <div>Finished at: </div>
+            </div>
+          </div>
+        );
       })}
     </>
   );
