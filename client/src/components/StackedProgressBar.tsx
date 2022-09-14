687e8b7370419db0befe564c9f0f7889fddd6392
Keerysanth Sribaskaran <keerush@live.com>
2022-09-14 16:53:54 -0400

    Create StackedProgressBar component

---
+++ client/src/components/StackedProgressBar.tsx
@@ -0,0 +1,36 @@
+import { Typography } from '@mui/material';
+import { ProgressData } from 'pages/pot-instance/models';
+import styles from './styles.module.css';
+
+interface Props {
+  data: ProgressData[];
+}
+const WIDTH = 90;
+const StackedProgressBar = ({ data }: Props) => {
+  return (
+    <>
+      <div className={styles.progressBarContainer}>
+        {data.map((section) => {
+          const innerStyle = {
+            'background-color': section.color,
+            right: `${(section.value / 100) * WIDTH}vw`
+          };
+
+          return (
+            <div
+              key={section.id}
+              className={styles.progressBarItem}
+              style={innerStyle}
+            />
+          );
+        })}
+      </div>
+      <div className={styles.legend}>
+        <Typography>Raw</Typography>
+        <Typography>Cooked</Typography>
+      </div>
+    </>
+  );
+};
+
+export default StackedProgressBar;
