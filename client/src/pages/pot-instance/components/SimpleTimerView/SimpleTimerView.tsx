78ec1eaf613fc7f337d3e99fd0d8c4d6e21c41cc
Keerysanth Sribaskaran <keerush@live.com>
2022-09-16 21:59:26 -0400

    Simplifed logic for SimpleTimerView

--- client/src/pages/pot-instance/components/SimpleTimerView/SimpleTimerView.tsx
+++ client/src/pages/pot-instance/components/SimpleTimerView/SimpleTimerView.tsx
@@ -2,6 +2,7 @@
 /* eslint-disable no-unused-vars */
 import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import StackedProgressBar from 'components/StackedProgressBar';
+import FoodLegendList from 'pages/pot-instance/components/SimpleTimerView/FoodLegendList';
 import { PotContent, ProgressData } from 'pages/pot-instance/models';
 import getRGBPerCategory from 'utils/colors';
 
@@ -10,38 +11,32 @@ interface Props {
   setPotContent: Dispatch<SetStateAction<PotContent[]>>;
 }
 
-interface Data extends PotContent {
-  color: string;
-}
-
 const SimpleTimerView = ({ potContent, setPotContent }: Props) => {
-  console.log(setPotContent);
-  const [data, setData] = useState<Map<string, Data>>(new Map());
-
-  const [outputData, setOutData] = useState<ProgressData[]>([]);
-  useEffect(() => {
-    potContent.forEach((potItem) => {
-      data.set(potItem.id, {
-        ...potItem,
-        color: data.get(potItem.id)?.color || getRGBPerCategory()
-      });
-    });
-    // TODO: Clean up renmoved items
-    setData(new Map(data));
-  }, [potContent, setData]);
+  const [outputData, setOutData] = useState<Record<string, ProgressData>>({});
 
   useEffect(() => {
-    const temp = Array.from(data.values()).map((d) => ({
-      id: d.id,
-      name: d.name,
-      color: d.color,
-      value: (d.timeLeft / d.cookTime) * 100
-    }));
+    // If item is new to the pot generate a new color if not use old color.
+    const updatedData: Record<string, ProgressData> = Object.fromEntries(
+      potContent.map(({ id, ...details }) => [
+        id,
+        {
+          id,
+          name: details.name,
+          color: outputData[id]?.color || getRGBPerCategory(),
+          value: (details.timeLeft / details.cookTime) * 100
+        }
+      ])
+    );
 
-    setOutData(temp);
-  }, [data, setOutData]);
+    setOutData(updatedData);
+  }, [potContent]);
 
-  return <StackedProgressBar data={outputData} />;
+  return (
+    <>
+      <StackedProgressBar data={Object.values(outputData)} />
+      <FoodLegendList colorMap={Object.values(outputData)} />
+    </>
+  );
 };
 
 export default SimpleTimerView;
